b733b2ca32dfbf80cb8a712acdaee251db3ad08d
ho_ong8 <honghj8959@gmail.com>
2023-02-19 23:21:02 +0900

    feat: add list, detail, update, delete function

--- src/main/java/com/sample/board/controller/MemberController.java
+++ src/main/java/com/sample/board/controller/MemberController.java
@@ -4,11 +4,14 @@ import com.sample.board.dto.MemberDTO;
 import com.sample.board.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,6 +29,44 @@ public class MemberController {
     public String join(@ModelAttribute MemberDTO memberDTO) {
         memberService.join(memberDTO);
         return "/member/login";
+    }
+
+    // 회원목록
+    @GetMapping("/member/")
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        model.addAttribute("memberList", memberDTOList);
+        return "/member/list";
+    }
+
+    // 회원정보 상세조회
+    @GetMapping("/member/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        MemberDTO memberDTO = memberService.findById(id);
+        model.addAttribute("member", memberDTO);
+        return "/member/detail";
+    }
+
+    // 회원정보 수정
+    @GetMapping("/member/update")
+    public String updateForm(HttpSession session, Model model) {
+        String myEmail = (String) session.getAttribute("loginEmail");
+        MemberDTO memberDTO = memberService.updateForm(myEmail);
+        model.addAttribute("updateMember", memberDTO);
+        return "/member/update";
+    }
+
+    @PostMapping("/member/update")
+    public String update(@ModelAttribute MemberDTO memberDTO) {
+        memberService.update(memberDTO);
+        return "redirect:/member/" + memberDTO.getId();
+    }
+
+    // 회원정보 삭제
+    @GetMapping("/member/delete/{id}")
+    public String deleteById(@PathVariable Long id) {
+        memberService.deleteById(id);
+        return "redirect:/member/";
     }
 
     // 로그인
